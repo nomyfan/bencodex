ede5a41805ee3e6031d068d102ed8fee2d68f838
Kim Chan <Nomyfan@live.com>
2020-04-14 15:01:02 +0800

    test: add edge cases (#1)
    * test: primitive int cases
    
    * test: primitive string
    
    * test: list
    
    * test: map
    
    * fix: typos

--- src/lib.rs
+++ src/lib.rs
@@ -96,100 +96,106 @@ pub fn parse(input: &str) -> Result<BNode, String> {
 }
 
 fn internal_parse(input: &str) -> Result<(BNode, usize), String> {
+    if input.len() < 2 {
+        return Err(format!("Input's length should >= 2, {}", input));
+    }
     let desc = input.chars().next().unwrap();
     match desc {
         'l' => parse_list(input),
         'd' => parse_map(input),
         '0'..='9' => parse_string(input),
         'i' => parse_int(input),
-        _ => Err("Undefined stopword".to_string()),
+        _ => Err(format!("Undefined delimiter: {}", desc)),
     }
 }
 
 fn parse_int(input: &str) -> Result<(BNode, usize), String> {
     let mut val: i64 = 0;
-    let mut positive = true;
+    let mut mul = 1;
     let mut next: usize = 1;
     for c in input.chars().skip(1) {
+        match c {
+            '-' if next == 1 => {
+                mul = -1;
+            }
+            'e' if next != 1 => {
+                return Ok((BNode::Int(val * mul), next + 1));
+            }
+            '0'..='9' => val = val * 10 + (c.to_digit(10).unwrap()) as i64,
+            _ => return Err(format!("Invalid number, {}", input)),
+        }
         next = next + 1;
-        if c == 'e' {
-            break;
-        }
-        if c == '-' {
-            positive = false;
-        }
-        match c.to_digit(10) {
-            Some(d) => val = val * 10 + d as i64,
-            None => return Err("Invalid integer".to_string()),
-        }
     }
 
-    if !positive {
-        val = -val;
-    }
-
-    Ok((BNode::Int(val), next))
+    Err(format!("Missing ending 'e', {}", input))
 }
 
 fn parse_string(input: &str) -> Result<(BNode, usize), String> {
-    let mut len = 0;
-    let mut next = 1;
+    let mut len: usize = 0;
+    let mut next: usize = 1;
     for c in input.chars() {
-        if c == ':' {
-            break;
+        match c {
+            ':' => {
+                return if next + len > input.len() {
+                    Err(format!(
+                        "String's length is shorter than expected, {}",
+                        input
+                    ))
+                } else {
+                    let mut ret = String::new();
+                    ret.push_str(&input[next..(next + len)]);
+                    Ok((BNode::Str(ret), next + len))
+                }
+            }
+            '0'..='9' => len = len * 10 + c.to_digit(10).unwrap() as usize,
+            _ => return Err(format!("Bad string is given, {}", input)),
         }
         next = next + 1;
-        match c.to_digit(10) {
-            Some(d) => len = len * 10 + d,
-            None => return Err("Bad string length is given".to_string()),
-        }
     }
 
-    let mut ret = String::new();
-    ret.push_str(&input[next..(next + len as usize)]);
-    next = next + len as usize;
-
-    Ok((BNode::Str(ret), next))
+    Err(format!("Missing ':' in the string, {}", input))
 }
 
 fn parse_list(input: &str) -> Result<(BNode, usize), String> {
     let mut nodes = vec![];
     let mut next = 1;
-    loop {
+    while next < input.len() {
+        if "e" == &input[next..next + 1] {
+            return Ok((BNode::List(nodes), next + 1));
+        }
         let (node, n) = internal_parse(&input[next..])?;
         next = next + n;
         nodes.push(node);
-        if "e" == &input[next..next + 1] {
-            next = next + 1;
-            break;
-        }
     }
 
-    Ok((BNode::List(nodes), next))
+    Err(format!("Missing 'e' at the end of list, {}", input))
 }
 
 fn parse_map(input: &str) -> Result<(BNode, usize), String> {
     let mut map = BMap::new();
     let mut next = 1;
-    loop {
+    while next < input.len() {
+        if "e" == &input[next..next + 1] {
+            return Ok((BNode::Map(map), next + 1));
+        }
         let (key_node, n) = internal_parse(&input[next..])?;
-        next = next + n;
         let key = match key_node {
             BNode::Str(s) => s,
-            _ => return Err("Dictionary key's type should be String".to_string()),
+            _ => {
+                return Err(format!(
+                    "Dictionary key's type should be String, {}",
+                    &input[next..]
+                ))
+            }
         };
+        next = next + n;
 
         let (val_node, n) = internal_parse(&input[next..])?;
         next = next + n;
         map.insert(key, val_node);
-
-        if "e" == &input[next..next + 1] {
-            next = next + 1;
-            break;
-        }
     }
 
-    Ok((BNode::Map(map), next))
+    Err(format!("Missing 'e' at the end of map, {}", input))
 }
 
 #[cfg(test)]
@@ -212,47 +218,77 @@ mod tests {
 
     #[test]
     fn test_primitive_int() {
-        use crate::BNode;
-        let result = crate::parse_int("i2147483648e");
-        match result {
-            Ok((bint, n1)) => match bint {
-                BNode::Int(i) => {
-                    assert_eq!(i, 2147483648);
-                    assert_eq!("i2147483648e".len(), n1);
+        let cases = vec!["i2147483648e", "i-253e"];
+        for x in &cases {
+            match crate::parse(x) {
+                Ok(bint) => {
+                    if let crate::BNode::Int(i) = bint {
+                        assert_eq!(i, x[1..x.len() - 1].parse::<i64>().unwrap());
+                    }
                 }
-                _ => panic!("Wrong type"),
-            },
-            Err(e) => panic!(e),
+                Err(e) => panic!(e),
+            }
+        }
+    }
+
+    #[test]
+    fn test_primitive_int_failed() {
+        let cases = vec!["i2522", "ie", "i", "i-12-3e", "i13ee"];
+        for x in &cases {
+            match crate::parse(x) {
+                Ok(_) => panic!("Should fail"),
+                Err(_) => (),
+            }
         }
     }
 
     #[test]
     fn test_primitive_string() {
         use crate::BNode;
-        let result = crate::parse_string("5:hello");
-        match result {
-            Ok((node, next)) => match node {
-                BNode::Str(v) => {
-                    assert_eq!("hello", &v[..]);
-                    assert_eq!(7, next);
+        let cases = vec!["4:halo"];
+        for x in &cases {
+            match crate::parse(x) {
+                Ok(node) => {
+                    if let BNode::Str(v) = node {
+                        let index = x.find(':').unwrap();
+                        assert_eq!(&x[index + 1..], &v[..]);
+                    }
                 }
-                _ => panic!("Wrong type"),
-            },
-            Err(e) => panic!(e),
+                Err(e) => panic!(e),
+            }
+        }
+    }
+
+    #[test]
+    fn test_primitive_string_failed() {
+        let cases = vec!["5:hello2", "5:halo", "521"];
+        for x in &cases {
+            match crate::parse(x) {
+                Ok(_) => panic!("Should fail"),
+                Err(_) => (),
+            }
         }
     }
 
     #[test]
     fn test_list() {
-        let input = "l4:spami42ee";
-        let len = input.len();
-        let result = crate::parse_list(&input);
-        match result {
-            Ok((node, next)) => {
-                assert_eq!(len, next);
-                assert_eq!(&format!("{}", node), &input);
+        let cases = vec!["l4:spami42ee", "le"];
+        for x in &cases {
+            match crate::parse(x) {
+                Ok(node) => assert_eq!(x, &format!("{}", node)),
+                Err(e) => panic!(e),
             }
-            Err(e) => panic!(e),
+        }
+    }
+
+    #[test]
+    fn test_list_failed() {
+        let cases = vec!["l4:halo"];
+        for x in &cases {
+            match crate::parse(x) {
+                Ok(_) => panic!("Should fail"),
+                Err(_) => (),
+            }
         }
     }
 
@@ -273,32 +309,42 @@ mod tests {
 
     #[test]
     fn test_map() {
-        let input =
-            "d8:announce41:http://bttracker.debian.org:6969/announce13:creation datei15739038104ee";
-        let len = input.len();
-        let result = crate::internal_parse(&input);
-
-        match result {
-            Ok((node, next)) => {
-                assert_eq!(len, next);
-                assert_eq!(&format!("{}", node), &input);
+        let cases = vec![
+            "d8:announce41:http://bttracker.debian.org:6969/announce13:creation datei15739038104ee",
+        ];
+        for x in &cases {
+            match crate::parse(x) {
+                Ok(node) => {
+                    assert_eq!(x, &format!("{}", node));
+                }
+                Err(e) => panic!(e),
             }
-            Err(e) => panic!(e),
+        }
+    }
+
+    #[test]
+    fn test_map_failed() {
+        let cases = vec!["d4:haloi23e", "di23e4:haloe"];
+        for x in &cases {
+            match crate::parse(x) {
+                Ok(_) => panic!("Should fail"),
+                Err(_) => (),
+            }
         }
     }
 
     #[test]
     fn test_nested_map() {
-        let input = r#"d8:announce41:http://bttracker.debian.org:6969/announce7:comment35:"Debian CD from cdimage.debian.org"13:creation datei1573903810e9:httpseedsl145:https://cdimage.debian.org/cdimage/release/10.2.0//srv/cdbuilder.debian.org/dst/deb-cd/weekly-builds/amd64/iso-cd/debian-10.2.0-amd64-netinst.iso145:https://cdimage.debian.org/cdimage/archive/10.2.0//srv/cdbuilder.debian.org/dst/deb-cd/weekly-builds/amd64/iso-cd/debian-10.2.0-amd64-netinst.isoe4:infod6:lengthi351272960e4:name31:debian-10.2.0-amd64-netinst.iso12:piece lengthi262144eee"#;
-        let len = input.len();
-        let result = crate::internal_parse(&input);
-
-        match result {
-            Ok((node, next)) => {
-                assert_eq!(len, next);
-                assert_eq!(&format!("{}", node), &input);
+        let cases = vec![
+            r#"d8:announce41:http://bttracker.debian.org:6969/announce7:comment35:"Debian CD from cdimage.debian.org"13:creation datei1573903810e9:httpseedsl145:https://cdimage.debian.org/cdimage/release/10.2.0//srv/cdbuilder.debian.org/dst/deb-cd/weekly-builds/amd64/iso-cd/debian-10.2.0-amd64-netinst.iso145:https://cdimage.debian.org/cdimage/archive/10.2.0//srv/cdbuilder.debian.org/dst/deb-cd/weekly-builds/amd64/iso-cd/debian-10.2.0-amd64-netinst.isoe4:infod6:lengthi351272960e4:name31:debian-10.2.0-amd64-netinst.iso12:piece lengthi262144eee"#,
+        ];
+        for x in &cases {
+            match crate::parse(x) {
+                Ok(node) => {
+                    assert_eq!(x, &format!("{}", node));
+                }
+                Err(e) => panic!(e),
             }
-            Err(e) => panic!(e),
         }
     }
 }
